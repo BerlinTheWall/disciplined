52f6701d1aacc4d3c4a31db0917450453bf7ac65
farnoodlotfali <farnoodlotfali@gmail.com>
2026-06-29 16:54:52 +0330

    adding dynamic-import

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,15 @@
+import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
+import dynamicImport from "vite-plugin-dynamic-import";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), dynamicImport(), tailwindcss()],
+  assetsInclude: ["**/*.md"],
+  resolve: {
+    alias: {
+      "@": path.join(__dirname, "src"),
+    },
+  },
 });
